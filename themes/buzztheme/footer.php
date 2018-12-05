c201c972e8233045a1125ac74726984efab5e58a
Chris Hanson <BlondyHanson@gmail.com>
2018-12-05 17:42:41 +0000

    Added footer with menu functionality

--- themes/buzztheme/footer.php
+++ themes/buzztheme/footer.php
@@ -10,9 +10,12 @@
 </div><!-- #content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-  <div class="site-info">
-
-  </div><!-- .site-info -->
+  <div class="logo">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/logo/buzz writing white.png" width="" height="" alt="buzz logo" />
+  </div><!-- .logo -->
+  <?php wp_nav_menu( array( 'theme_location' => 'new-menu', 'container_class' => 'footerlinks' ) ); ?>
+  <p class="copyright">2019 Copyright ©</p>
+  <?php wp_nav_menu( array( 'theme_location' => 'another-menu', 'container_class' => 'footersocialmedia' ) ); ?>
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
