e52c477bcdbda94477d3bd1448111e88527814c7
Jasmine Smith <jasmine.smith28@outlook.com>
2018-12-10 18:49:33 +0000

    started styling the studio page

--- themes/buzztheme/page-studio.php
+++ themes/buzztheme/page-studio.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-<section>		
+<section class="studio-users">		
 <?php
 $args1 = array(
  'role' => 'studio',
@@ -20,15 +20,15 @@ $args1 = array(
 );
  $studios = get_users($args1);
  foreach ($studios as $user) {
- echo '<li>'
+ echo '<div><div class="studio">'
  . get_avatar($user->ID, 120) .
- '<br />'
+ '</div><li>'
  . $user->display_name .
- '<br />'
+ '</li> <li>'
  . $user->user_email .
- '<br />'
+ '</li> <li>'
  . $user->user_description .
- '</li>';
+ '</li> </div>';
  }
 ?>
 
@@ -39,19 +39,3 @@ $args1 = array(
 
 
 <?php get_footer(); ?>
-
-<?php
-// Set the Current Author Variable $curauth
-$curauth = (isset($_GET['author_name'])) ? get_user_by('role=studio') : get_userdata(intval($author));
-?>
-     
-<div class="author-profile-card">
-    <h2>About: <?php echo $curauth->first_name; ?></h2>
-    <div class="author-photo">
-    <?php echo get_avatar( $curauth->user_email , '90 '); ?>
-    </div>
-    <p><strong>Website:</strong> <a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a><br />
-    <strong>Bio:</strong> <?php echo $curauth->user_description; ?></p>
-</div>
-     
-<h2>Posts by <?php echo $curauth->nickname; ?>:</h2>
