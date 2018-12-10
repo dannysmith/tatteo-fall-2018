2f7df02c7d125c8be8083647091690dd8b541d84
Jasmine Smith <jasmine.smith28@outlook.com>
2018-12-10 18:12:53 +0000

    deleted extra code

--- themes/buzztheme/page-artists.php
+++ themes/buzztheme/page-artists.php
@@ -39,19 +39,3 @@ $args1 = array(
 
 
 <?php get_footer(); ?>
-
-<?php
-// Set the Current Author Variable $curauth
-$curauth = (isset($_GET['author_name'])) ? get_user_by('role=studio') : get_userdata(intval($author));
-?>
-     
-<div class="author-profile-card">
-    <h2>About: <?php echo $curauth->first_name; ?></h2>
-    <div class="author-photo">
-    <?php echo get_avatar( $curauth->user_email , '90 '); ?>
-    </div>
-    <p><strong>Website:</strong> <a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a><br />
-    <strong>Bio:</strong> <?php echo $curauth->user_description; ?></p>
-</div>
-     
-<h2>Posts by <?php echo $curauth->nickname; ?>:</h2>
