2a82f8cb6a58c46db36379a131e3b4184198584a
Maya Ananina <maya.ananina@gmail.com>
2018-12-11 14:37:55 +0000

    Styling guestpots page

--- themes/buzztheme/page-my-guestspots.php
+++ themes/buzztheme/page-my-guestspots.php
@@ -9,28 +9,28 @@ get_header();?>
 
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
-    <h1>My Guestspot</h1>
-    <?php $args = array('post_type' => 'guestspot');
-$guestspots = new WP_Query($args); /* $args set above*/?>
-    <?php if ($guestspots->have_posts()): ?>
-    <?php while ($guestspots->have_posts()): $guestspots->the_post();?>
     <section class="guestspot-section">
-      <h1>
-        <?php the_title();?>
-      </h1>
-      <p>
-        <?php echo CFS()->get('location'); ?>
-      </p>
-      <section>
-        <?php endwhile;?>
-        <?php the_posts_navigation();?>
-        <?php wp_reset_postdata();?>
-        <?php else: ?>
-        <h2>Nothing found!</h2>
-        <?php endif;?>
-
-
-
+      <h1>My Guestspot</h1>
+      <?php $args = array('post_type' => 'guestspot');
+$guestspots = new WP_Query($args); /* $args set above*/?>
+      <?php if ($guestspots->have_posts()): ?>
+      <?php while ($guestspots->have_posts()): $guestspots->the_post();?>
+      <div class="guestspot">
+        <h1>
+          <?php echo CFS()->get('studio_name'); ?>
+        </h1>
+        <p>
+          <?php echo CFS()->get('location'); ?>
+        </p>
+      </div>
+      <?php endwhile;?>
+      <?php the_posts_navigation();?>
+      <?php wp_reset_postdata();?>
+      <?php else: ?>
+      <h2>Nothing found!</h2>
+      <?php endif;?>
+      <div class="new-guestspot"></div>
+    </section>
   </main><!-- #main -->
 </div><!-- #primary -->
 <?php get_footer();?>
