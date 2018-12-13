0239b1023bc3d4d50e4db33560342d28ed1ca91f
Jasmine Smith <jasmine.smith28@outlook.com>
2018-12-13 12:21:36 +0000

    deleted search function

--- themes/buzztheme/template-parts/content-search.php
+++ themes/buzztheme/template-parts/content-search.php
@@ -13,20 +13,13 @@
 	
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() && 'user' === get_users() ) : ?>
+		<?php if ( 'post' === get_post_type()) : ?>
 		<div class="entry-meta">
 			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	
-<?php
-$blogusers = get_users( array( 'search' => 'john' ) );
-// Array of WP_User objects.
-foreach ( $blogusers as $user ) {
-    echo '<span>' . esc_html( $user->user_email ) . '</span>';
-} ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
