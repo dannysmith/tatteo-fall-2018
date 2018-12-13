9e9c76b13eac87f1a419cde5b7957759014ad91f
Paul Petillot <petillotpaul@gmail.com>
2018-12-13 18:23:43 +0000

    Search filter for guestspots adding

--- themes/buzztheme/js/guestspot-search.js
+++ themes/buzztheme/js/guestspot-search.js
@@ -1,14 +1,15 @@
 (function ($) {
   $('.form-guest-search').on('submit', function (event) {
+    const $guestspotsContainer = $('.guestspots-container-js')
     event.preventDefault();
     let data = {
       location: $("#location").val(),
       start_date: $('#start-date').val(),
       finish_date: $('#finish-date').val(),
     }
-   let locationLow = data.location.toLowerCase();
-   let startDateChoosen = new Date(data.start_date);
-   let finishDateChoosen = new Date(data.finish_date);
+    let locationLow = data.location.toLowerCase();
+    let startDateChoosen = new Date(data.start_date);
+    let finishDateChoosen = new Date(data.finish_date);
     $.ajax({
         method: 'GET',
         url: api_vars.root_url + 'wp/v2/guestspots-api',
@@ -19,20 +20,25 @@
       })
       .done(function (response) {
         event.preventDefault();
-        console.log(response)
+        $guestspotsContainer.empty();
         for (let i = 0; i < response.length; i++) {
           let locationJson = response[i].location.toLowerCase();
           let startDateJson = new Date(response[i].start_date);
-          let finishDateJson = new Date (response[i].finish_date);
-          if (locationLow == locationJson ){
-            if (startDateChoosen >= startDateJson && finishDateChoosen<= finishDateJson){
-              console.log('Hey')
+          let finishDateJson = new Date(response[i].finish_date);
+          let image = response[i].image;
+          let title = response[i].title.rendered;
+          if (locationLow == locationJson) {
+            if (startDateChoosen >= startDateJson && finishDateChoosen <= finishDateJson) {
+              $guestspotsContainer.empty();
+              $guestspotsContainer.html(`<div><img src="${image}"><h2>${title}</h2>
+              <p>${data.location}</p>`)
             }
           }
         }
       })
       .fail(function () {
-        //post and alert with failure var from functions.php
+        $guestspotsContainer.empty();
+        $guestspotsContainer.html('Sorry, try again...')
       });
   })
 })(jQuery);
