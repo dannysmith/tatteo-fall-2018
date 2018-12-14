81afacac8d2018adf74a49aeb12bfd0d2fea82fb
Maya Ananina <maya.ananina@gmail.com>
2018-12-14 11:35:47 +0000

    Styling new guestpot page

--- themes/buzztheme/page-create-guestspot.php
+++ themes/buzztheme/page-create-guestspot.php
@@ -8,41 +8,39 @@
 get_header();?>
 
 <div id="primary" class="content-area edit-area">
-  <main id="main" class="site-main" role="main">
-    <section class="create-guestspot-section">
+  <section class="create-guestspot-section">
 
-      <form method='post' class='image-upload-form'>
-        <h1>New Guestspot</h1>
-        <input type='file' id='guespot-image' required>
+    <form method='post' class='image-upload-form'>
+      <h1>New Guestspot</h1>
+      <input type='file' id='guespot-image' required>
 
-        <?php global $current_user;
+      <?php global $current_user;
 get_currentuserinfo();?>
 
-        <p><label for="studio-name">Studio Name</label><br />
-          <input type="text" id="studio-name" value="<?php echo $current_user->user_login ?>" />
-        </p>
-        <p>
-          <label for="location">Location</label><br />
-          <input type="text" id="location" required />
-        </p>
-        <p><label for="guestspot-start">Start Date</label><br />
-          <input type="date" id="start-date" required />
-        </p>
-        <p><label for="guestspot-finish">Finish Date </label><br />
-          <input type="date" id="finish-date" required />
-        </p>
-        <p>
-        </p>
-        <input type='submit' name='Submit' value="Add Guestspot" class='upload-btn'>
-      </form>
-      <div class="imagecontainer">
-        <img src="../../../../buzz/wp-content/themes/buzztheme/assets/Images/Textures-2-Vertical.png" alt="sidetexture">
-      </div>
-    </section>
+      <p><label for="studio-name">Studio Name</label><br />
+        <input type="text" id="studio-name" value="<?php echo $current_user->user_login ?>" />
+      </p>
+      <p>
+        <label for="location">Location</label><br />
+        <input type="text" id="location" required />
+      </p>
+      <p><label for="guestspot-start">Start Date</label><br />
+        <input type="date" id="start-date" required />
+      </p>
+      <p><label for="guestspot-finish">Finish Date </label><br />
+        <input type="date" id="finish-date" required />
+      </p>
+      <p>
+      </p>
+      <input type='submit' name='Submit' value="Add Guestspot" class='upload-btn'>
+    </form>
+    <div class="imagecontainer">
+      <img src="../../../../buzz/wp-content/themes/buzztheme/assets/Images/Textures-2-Vertical.png" alt="sidetexture">
+    </div>
+  </section>
 
-    <h1>
-      <?php echo get_the_author_meta('ID'); ?>
-    </h1>
-  </main><!-- #main -->
+  <h1>
+    <?php echo get_the_author_meta('ID'); ?>
+  </h1>
 </div><!-- #primary -->
 <?php get_footer();?>
