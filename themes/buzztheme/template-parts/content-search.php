c5d7100dda8c1e5e6d9d0967e49633b53902380f
Jasmine Smith <jasmine.smith28@outlook.com>
2018-12-14 11:35:40 +0000

    styled search

--- themes/buzztheme/template-parts/content-search.php
+++ themes/buzztheme/template-parts/content-search.php
@@ -8,23 +8,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-
 
 		<img src="<?php echo CFS()->get('image'); ?>" />
-	
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
+	<div class="entry-summary">
+	<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-
-	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 
 	</div><!-- .entry-summary -->
