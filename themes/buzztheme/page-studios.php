c9e6b22002c962de4b0a7846d97362202efd3bda
Maya Ananina <maya.ananina@gmail.com>
2018-12-19 12:05:19 +0000

    Loading studios

---
+++ themes/buzztheme/page-studios.php
@@ -0,0 +1,48 @@
+<?php /* Template name: page-studios */?>
+
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header();?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+    <section class="title">
+      <h1>Studios</h1>
+    </section>
+    <section class="studio-users">
+
+      <?php
+$args1 = array(
+    'role' => 'studio',
+    'orderby' => 'registered',
+    'order' => 'desc',
+    'number' => '6',
+);
+$studios = get_users($args1);
+foreach ($studios as $user) {
+    echo '<div class="container"><div class="studio">' .
+    '<a href="' . get_author_posts_url($user->ID) . '">'
+    . get_avatar($user->ID, 120) .
+    '</div><li>'
+    . $user->display_name .
+    '</li> <li>'
+    . $user->location .
+        '</li> </div></a>';
+}
+?>
+
+    </section>
+
+    <section class="button"><button class="load-more-studios">Load more</button></section>
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+
+<?php get_footer();?>
