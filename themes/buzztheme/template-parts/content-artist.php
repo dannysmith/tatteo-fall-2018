e66aa622f23d168eb4873becfb67505b8bf8d12a
Chris Hanson <BlondyHanson@gmail.com>
2018-12-20 17:14:06 +0000

    Changed code in content-studio to match Mayas, changed styling in single studio. Changed styling for single artist and included social media

--- themes/buzztheme/template-parts/content-artist.php
+++ themes/buzztheme/template-parts/content-artist.php
@@ -13,19 +13,28 @@
 	</header><!-- .entry-header -->
 
 	<section class="single-artist">
-	<?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));?>
 	<div class="avatar"> <?php echo get_avatar($user->ID, 120) ?></div>
+	<div class="artistdetails">
+	<?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));?>
 	<div class="artistname"><h2><?php echo $curauth->nickname; ?></h2></div>
 	<?php if(!empty($curauth->user_description)) { ?>
 		<div class="artistdescription"><p><?php echo $curauth->user_description; ?></p></div>
 	<?php } else { ?>
 		<div class="artistdescription" <?php echo 'hidden'; ?>></div>
 	<?php } ?>
-	</section>
-	<section class="instagram">
-	<h2>Instagram</h2>
-	<div class="grid container">
-<a href="https://www.instagram.com/<?php echo $curauth->instagram ?>"><?php echo $curauth->instagram ?></a>
+	<?php if(!empty($curauth->facebook&&$curauth->instagram)) { ?>
+		<div class="socialmedia">
+		<div class="socialmedialinks">
+		<ul>
+		<li><a href="https://www.instagram.com/<?php echo $curauth->instagram ?>"><i class="fa fa-instagram"></i><?php echo $curauth->instagram ?></a></li>
+		<li><a href="https://www.facebook.com/<?php echo $curauth->facebook ?>"><i class="fa fa-facebook"></i><?php echo $curauth->facebook ?></a></li>
+		</ul>
+		</div>
+		</div> 
+	<?php } else { ?>
+		<div class="socialmedia" <?php echo 'hidden'; ?>></div>
+	<?php } ?>
+
 	</div>
 	</section>
 
--- themes/buzztheme/template-parts/content-studio.php
+++ themes/buzztheme/template-parts/content-studio.php
@@ -18,6 +18,18 @@
     ?>
 	<div class="avatar" style="background-image: url(<?php echo get_avatar_url($user->ID)?>);"></div>
 	<div class="studioname"><h2><?php echo $curauth->nickname; ?></h2></div>
+	<?php if(!empty($curauth->facebook&&$curauth->instagram)) { ?>
+		<div class="socialmedia">
+		<div class="socialmedialinks">
+		<ul>
+		<li><a href="https://www.instagram.com/<?php echo $curauth->instagram ?>"><i class="fa fa-instagram"></i><?php echo $curauth->instagram ?></a></li>
+		<li><a href="https://www.facebook.com/<?php echo $curauth->facebook ?>"><i class="fa fa-facebook"></i><?php echo $curauth->facebook ?></a></li>
+		</ul>
+		</div>
+		</div>
+	<?php } else { ?>
+		<div class="socialmedia" <?php echo 'hidden'; ?>></div>
+	<?php } ?>
 	<?php if(!empty($curauth->user_description)) { ?>
 		<div class="studiodescription"><p><?php echo $curauth->user_description; ?></p></div>
 	<?php } else { ?>
@@ -26,38 +38,36 @@
 	</section>
 
 	<section class="previous-guestspots">
-	<h2>Previous Guestspots</h2>
-	<div class="grid-container">
-	<?php
-    $loop = new WP_Query( array( 'post_type' => 'guestspot', 'order' => 'ASC', 'posts_per_page' => '3') );
-    if ( $loop->have_posts() ) :
-        while ( $loop->have_posts() ) : $loop->the_post(); ?>
-            <div class="guestspots">
-                <?php if ( has_post_thumbnail() ) { ?>
-                    <div class="link-guestspot">
-                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-                    </div>
-                <?php } ?>
-                <div class="">
-                    <p><?php echo get_the_title(); ?></p>
-                </div>
-			</div>
-		<?php endwhile;
-    endif;
-    wp_reset_postdata();
+    <?php $args = array('post_type' => 'guestspot', 'author' => $curauth->ID, 'order' => 'ASC', 'posts_per_page' => '3');
+	$guestspots = new WP_Query($args);
+	if ($guestspots->have_posts()): ?>
+    <h2>Previous Guestspots</h2>
+    <div class="grid-container">
+      <?php while ($guestspots->have_posts()): $guestspots->the_post();?>
+      <div id='<?php echo get_the_ID() ?>' class="guestspots">
+        <img src=" <?php echo CFS()->get('image'); ?>" />
+
+        <div class="studio-information">
+          <a href="<?php echo get_post_permalink(get_the_ID()) ?>">
+            <p>
+              <?php echo CFS()->get('studio_name'); ?>
+            </p>
+          </a>
+          <p>
+            <?php echo CFS()->get('location'); ?>
+          </p>
+          <p>
+			<?php get_currentuserinfo(); ?>
+			<a href="https://www.instagram.com/<?php echo $curauth->instagram ?>"><?php echo $curauth->instagram ?></a>
+          </p>
+        </div>
+
+    </div>
+    <?php endwhile;?>
+    <?php endif;
+wp_reset_postdata();
 ?>
-	</div>
-	</section>
-
-	<section class="instagram">
-	<h2>Instagram</h2>
-	<div class="grid container"></div>
-	</section>
-
-
-	<section class="studio-location">
-	<h2>Location</h2>
-	<p><?php echo $curauth->location; ?></p>
-	</section>
+    </div>
+  </section>
 
 </article><!-- #post-## -->
